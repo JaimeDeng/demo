f7bd71692c0bdead5d2c6894f6f0cbbc0f92be25
Jaime <raven0980642580@gmail.com>
2023-03-17 01:33:13 +0800

    test

--- src/main/java/practice/Practice24_6_TreeMap.java
+++ src/main/java/practice/Practice24_6_TreeMap.java
@@ -20,7 +20,7 @@ public class Practice24_6_TreeMap {
 		map.put(30 , "香蕉");
 		map.put(50 , "芒果");
 		map.put(20 , "西瓜");
-		map.put(150 , "酪梨");
+		map.put(200 , "酪梨");
 		//用.get(key)獲取該key的value
 		System.out.println("最便宜的水果:" + map.get(map.firstKey()) + " 單價:" + map.firstKey());
 		System.out.println("最貴的水果:" + map.get(map.lastKey()) + " 單價:" + map.lastKey());
